be7796cacf972e2fccdbb6b4da9ef1b325fc415d
thanhdt246 <dothanh24062000@gmail.com>
2020-11-20 13:59:00 +0700

    upload exercise

---
+++ src/main/java/controller/CustomerController.java
@@ -0,0 +1,39 @@
+package controller;
+
+import dao.ICustomerDAO;
+import model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+    @Autowired
+    private ICustomerDAO customerDAO;
+
+    @GetMapping("/")
+    public ModelAndView showList(){
+        return new ModelAndView("index","customers",customerDAO.findAll());
+    }
+
+    @GetMapping("/edit/{id}")
+    public ModelAndView edit(@PathVariable Long id){
+        return new ModelAndView("edit","customer",customerDAO.findOne(id));
+    }
+
+    @PostMapping("/edit")
+    public ModelAndView edit(@ModelAttribute Customer customer, RedirectAttributes redirectAttributes){
+        customerDAO.save(customer);
+        redirectAttributes.addFlashAttribute("editSuccess","Saved customer successfully!");
+        return new ModelAndView("redirect:/customer/edit/"+customer.getId());
+    }
+
+    @GetMapping("/delete/{id}")
+    public ModelAndView delete(@PathVariable Long id, RedirectAttributes redirectAttributes){
+        customerDAO.delete(id);
+        return new ModelAndView("redirect:/customer/");
+    }
+}
